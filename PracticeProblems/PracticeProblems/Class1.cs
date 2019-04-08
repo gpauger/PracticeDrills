12303dddbd7dce579b100ea164c3ef536b75d701
Greg Auger <gpauger@gmail.com>
2019-04-08 12:26:30 -0700

    initial commit add all files

---
+++ PracticeProblems/PracticeProblems/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class Class1
+    {
+        public static String LongestConsec(string[] strarr, int k)
+        {
+            string bar = "";
+            foreach (string str in strarr)
+            {
+                string result = String.Join("", strarr, Array.IndexOf(strarr, str), k);
+                if (result.Count() > bar.Count())
+                { bar = result; }
+            }
+            return bar;
+        }
+    }
+}
---
+++ PracticeProblems/PracticeProblemsTests/Class1Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public static class LongestConsecutivesTests
+{
+    private static void testing(string actual, string expected)
+    {
+        Assert.AreEqual(expected, actual);
+    }
+    [Test]
+    public static void test1()
+    {
+        Console.WriteLine("Basic Tests");
+        testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
+        testing(LongestConsecutives.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
+        testing(LongestConsecutives.LongestConsec(new String[] { }, 3), "");
+        testing(LongestConsecutives.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+        testing(LongestConsecutives.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+        testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
+        testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
+        testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
+        testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
+    }
+}
